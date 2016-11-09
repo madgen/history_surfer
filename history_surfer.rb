57c4307c0f9af1f6dd7ce606d07e60f9713d70d6
Mistral Contrastin <me@madgen.net>
2016-11-09 14:25:45 +0000

    Add proper dispatch mechanism for commands

--- history_surfer.rb
+++ history_surfer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
+
 require 'pathname'
 require 'minitest'
+require 'English'
 
 ROOT = Pathname.new ARGV[0]
 Dir.chdir ROOT
@@ -9,7 +11,20 @@ FILE_NAME = ARGV[1]
 LOG_R = /commit (\w+)$.*?@@ -\d+,\d+ \+(\d+),(\d+) @@/m
 STENCIL_R = /^\((\d+):+\d+\)-\((\d+):\d+\) \t(.*)$/
 
-`git checkout master`
+DISPATCH = lambda do |command|
+  Tempfile.open do |tmp|
+    pid = spawn command, out: tmp.path, err: File::NULL
+    Process.wait pid
+    tmp.flush
+    [$CHILD_STATUS.success?, tmp.read]
+  end
+end
+
+DISPATCH.call 'git checkout master'
+
+at_exit do
+  DISPATCH.call 'git checkout master'
+end
 
 # Stencil specification representation
 class StencilSpec
@@ -55,12 +70,12 @@ class Commit
   private_class_method
 
   def self.collect_specs(sha)
-    `git checkout #{sha}^`
+    DISPATCH.call("git checkout #{sha}")
 
     if @cache[sha]
       @cache[sha]
     else
-      output = `camfort stencils-infer #{FILE_NAME}`
+      _, output = DISPATCH.call("camfort stencils-infer #{FILE_NAME}")
 
       @cache[sha] =
         output.scan(STENCIL_R).map do |lb, le, spec|
@@ -71,18 +86,21 @@ class Commit
   end
 end
 
-head = Commit.new `git rev-parse HEAD`.chomp
+head = Commit.new DISPATCH.call('git rev-parse HEAD')[1].chomp
 
 chains =
   head.specs.map do |topspec|
-    output = `git log -L #{topspec.lbegin},#{topspec.lend}:#{FILE_NAME}`
+    DISPATCH.call('git checkout master')
+    _, output = DISPATCH.call("git log -L #{topspec.lbegin},#{topspec.lend}:#{FILE_NAME}")
 
     chain = [[head.sha, topspec]]
     output.scan(LOG_R) do |sha, lb, size|
       lbegin = lb.to_i
       commit = Commit.new sha
       spec = commit.search(lbegin, lbegin + size.to_i - 1)
-      raise "Stencil wasn't found for #{commit.sha}" unless spec
+      # If we cannot find any specs at the logged location we can't track
+      # further so we give up
+      break unless spec
 
       # Make the chain more compact if the spec remains the same.
       chain.pop if chain.last[1] == spec
@@ -99,5 +117,3 @@ chains.each do |chain|
     puts "#{sha}: #{spec}"
   end
 end
-
-`git checkout master`
